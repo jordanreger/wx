c0835921d8b643c220cc1444e78347a1ddabbd22
Jordan Reger <mail@jordanreger.com>
2024-03-07 10:56:39 -0500

    close #6

--- social/hashtags.go
+++ social/hashtags.go
@@ -1,15 +1,26 @@
 package social
 
-import "strings"
+import (
+	"strings"
+)
 
 func GetHashtags(area string) string {
-	var states []string
-
 	areas := strings.Split(area, "; ")
+	var states = make([]string, 0, len(areas))
 	for _, area := range areas {
-		states = append(states, "#"+strings.Split(area, ", ")[1]+"WX")
+		states = append(states, strings.Split(area, ", ")[1])
 	}
 
-	hashtags := strings.Join(states, " ")
-	return hashtags
+	var m = make(map[string]bool)
+	var hashtags = make([]string, 0, len(states))
+
+	for _, state := range states {
+		if _, ne := m[state]; !ne {
+			m[state] = true
+			hashtags = append(hashtags, "#"+state+"WX")
+		}
+	}
+
+	return strings.Join(hashtags, " ")
+
 }
---
+++ social/hashtags_test.go
@@ -0,0 +1,15 @@
+package social_test
+
+import (
+	"testing"
+
+	"github.com/fjalldev/wx/social"
+)
+
+func Test_GetHashtags(t *testing.T) {
+	hashtags := social.GetHashtags("Los Angeles, CA; Columbus, OH; Cleveland, OH; Columbus, OH; Columbus, SC")
+	t.Log(hashtags)
+	if hashtags != "#CAWX #OHWX #SCWX" {
+		t.Fatal("Had " + hashtags + ", wanted '#CAWX #OHWX #SCWX'")
+	}
+}
